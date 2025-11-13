48107d27db927b54984a2992a96f82082217190b
almamutuguti <alma.wanjiru8j@gmail.com>
2025-11-13 13:56:04 +0300

    add deploy link to cors

--- HavenBackend/settings.py
+++ HavenBackend/settings.py
@@ -80,6 +80,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
+    "https://haven-indol.vercel.app/",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
